ee3b7e3d2408cde19569c2e89159849cf3fa97be
lucas <lucas.menot@viacesi.fr>
2023-12-15 15:32:45 +0100

    Working Falling platforms

--- Assets/Scripts/World/Platform/FallingPlatform.cs
+++ Assets/Scripts/World/Platform/FallingPlatform.cs
@@ -12,8 +12,9 @@ public class FallingPlatform : MonoBehaviour
 
     void Start()
     {
-        originalPosition=transform.position;
-        originalRotation=transform.rotation;
+        anim=gameObject.GetComponent<Animation>();
+        originalPosition = this.transform.position;
+        originalRotation = this.transform.rotation;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,8 +24,9 @@ public class FallingPlatform : MonoBehaviour
             IPlayable player = other.GetComponent<IPlayable>();
             if (player!=null)
             {
+                isFalling=true;
                 StartCoroutine(FallAndRespawn());
-                isFalling = true;
+                
             }
         }
     }
@@ -42,28 +44,45 @@ public class FallingPlatform : MonoBehaviour
         }
     }
 
-    private IEnumerator FallAndRespawn()
+    // Plays an animation clip.
+    private void PlayAnimation(string animationName)
     {
-        yield return new WaitForSeconds(5f); // Attendre 5 secondes avant de faire tomber la plateforme
-
-        Fall();
-
-        yield return new WaitForSeconds(5f); // Attendre 5 secondes avant de réapparaître la plateforme
-        Respawn();
+        anim.Play(animationName);
     }
 
-    private void Fall()
+    private IEnumerator FallAndRespawn()
     {
-        // Ajouter le code pour faire tomber la plateforme (par exemple, désactiver le collider ou jouer une animation)
-        Debug.Log("Platform is falling!");
+        PlayAnimation("FallingPlatform"); // Play animation
+        yield return new WaitForSeconds(3f); // Wait 5 seconds making fall the platform
+
+        StartCoroutine(FallPlatform(10f)); // make the platform fall
+
+        yield return new WaitForSeconds(2f);
+        Respawn();
+        yield return new WaitForSeconds(1f);
+        isFalling=false;
+    }
+
+    private IEnumerator FallPlatform(float distanceToFall)
+    {
+        float elapsedTime = 0f;
+        Vector3 initialPosition = transform.position;
+        Vector3 targetPosition = originalPosition-new Vector3(0f, distanceToFall, 0f);
+
+        while (elapsedTime<1f)
+        {
+            transform.position=Vector3.Lerp(initialPosition, targetPosition, elapsedTime);
+            elapsedTime+=Time.deltaTime*0.5f; // Contrôlez la vitesse de descente ici
+            yield return null;
+        }
+
+        transform.position=targetPosition;
     }
 
     private void Respawn()
     {
-        // Ajouter le code pour réapparaître la plateforme à sa position d'origine
-        transform.position=originalPosition;
-        transform.rotation=originalRotation;
-        Debug.Log("Platform respawned!");
-        isFalling=false;
+        this.transform.position=originalPosition;
+        this.transform.rotation=originalRotation;
+        PlayAnimation("RespawningPlatform");
     }
 }
