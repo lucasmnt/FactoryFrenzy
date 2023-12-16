311efbd3f86639fb91ec75cdd12952993bd820f0
lucas <lucas.menot@viacesi.fr>
2023-12-16 12:41:46 +0100

    code cleanup

--- Assets/Scripts/World/PlayerSpawner.cs
+++ Assets/Scripts/World/PlayerSpawner.cs
@@ -6,6 +6,7 @@ public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField]
     public List<GameObject> playersToSpawn = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
