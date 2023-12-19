1b99572c6513b2118e0dcf0293952fb5233e5ac7
lucas <lucas.menot@viacesi.fr>
2023-12-19 19:25:28 +0100

    Turbine Update, fbx imports

--- Assets/Scripts/World/Traps/Turbine.cs
+++ Assets/Scripts/World/Traps/Turbine.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurbineTrap : MonoBehaviour
+public class Turbine : MonoBehaviour
 {
+    [SerializeField]
+    public Animation anim;
+    
+    [SerializeField]
+    public bool isBlowing=true;
 
     List<Rigidbody> RigidbodiesInWindZoneList = new List<Rigidbody>();
     public Vector3 windDirection = Vector3.right;
     public float windStrength = 5;
+
+    private void Start()
+    {
+        anim=gameObject.GetComponentInChildren<Animation>();
+        PlayAnimationLoop();
+    }
+
+    private IEnumerator PlayAnimationLoop()
+    {
+        while (isBlowing)
+        {
+            PlayAnimation("TurbineRunning");
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    // Plays an animation clip.
+    private void PlayAnimation(string animationName)
+    {
+        anim.Play(animationName);
+    }
 
     private void OnTriggerEnter(Collider col)
     {
