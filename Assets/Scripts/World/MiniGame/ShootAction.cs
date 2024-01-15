dfd6f02e21c0748ca9b88b847fb510e7102a1c1e
Andy31700 <dourel.andy@gmail.com>
2024-01-15 15:03:24 +0100

    Add new model for the base and the turret

--- Assets/Scripts/World/MiniGame/ShootAction.cs
+++ Assets/Scripts/World/MiniGame/ShootAction.cs
@@ -1,10 +1,9 @@
-﻿using System.Threading;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShootAction : MonoBehaviour
 {
-    public float limiteRotationHorizontale = 35f; // Ajustez selon vos besoins
-    public float limiteRotationVerticale = 15f;   // Ajustez selon vos besoins
+    public float limiteRotationVerticale = 15f;
+    public float limiteRotationHorizontale = 35f;
 
     private float rotationHorizontale = 0f;
     private float rotationVerticale = 0f;
@@ -13,22 +12,56 @@ public class ShootAction : MonoBehaviour
     public Transform barrel;
     public GameObject _projectile;
 
+    [SerializeField]
+    private Direction direction = Direction.Forward;
+
+    public enum Direction 
+    {
+       Left,
+       Right,
+       Forward,
+       Backward
+    }
+
+    // Ajout de la variable pour la rotation en Y de la tourelle
+    private float rotationY;
+
     void Update()
     {
-        // Obtenez les mouvements de la souris ou des axes de manette
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        // Appliquez la rotation horizontale et limitez-la
-        rotationHorizontale += mouseX;
-        rotationHorizontale = Mathf.Clamp(rotationHorizontale, -limiteRotationHorizontale, limiteRotationHorizontale);
+        switch (direction)
+        {
+            case Direction.Left:
+                rotationHorizontale += mouseX;
+                rotationHorizontale = Mathf.Clamp(rotationHorizontale, 235, 305);
+                break; 
+            case Direction.Right:
+                rotationHorizontale += mouseX;
+                rotationHorizontale = Mathf.Clamp(rotationHorizontale, 55, 125);
+                break; 
+            case Direction.Forward:
+                rotationHorizontale += mouseX;
+                rotationHorizontale = Mathf.Clamp(rotationHorizontale, -35, 35);
+                break;
+            case Direction.Backward:
+                rotationHorizontale += mouseX;
+                rotationHorizontale = Mathf.Clamp(rotationHorizontale, 145, 215);
+                break;
+            /*case default:
+                Debug.Log("Error");
+                break;*/
+        }
 
-        // Appliquez la rotation verticale et limitez-la
-        rotationVerticale -= mouseY; // Inversez la rotation verticale pour correspondre aux conventions Unity
+        rotationVerticale -= mouseY;
         rotationVerticale = Mathf.Clamp(rotationVerticale, -limiteRotationVerticale, limiteRotationVerticale);
 
-        // Appliquez les rotations à la caméra ou au personnage
-        transform.localRotation = Quaternion.Euler(rotationVerticale, rotationHorizontale, 0f);
+        // Ajout de la récupération de la rotation en Y de la tourelle
+        rotationY = barrel.rotation.eulerAngles.y;
+
+        head.localRotation = Quaternion.Euler(0f, 0f, -rotationVerticale);
+        transform.localRotation = Quaternion.Euler(0f, rotationHorizontale, 0f);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -38,8 +71,20 @@ public class ShootAction : MonoBehaviour
 
     void Shoot()
     {
-        GameObject clone = Instantiate(_projectile, barrel.position, head.rotation);
-        clone.GetComponent<Rigidbody>().AddForce(head.forward * 1500);
+        // Obtenir la rotation actuelle du canon
+        Quaternion currentBarrelRotation = barrel.rotation;
+
+        // Utiliser la rotation actuelle pour calculer la direction de tir
+        Vector3 shootingDirection = currentBarrelRotation * Vector3.right;
+
+        Debug.Log("Canon Rotation: " + barrel.rotation.eulerAngles);
+        Debug.Log("Projectile Rotation: " + barrel.rotation.eulerAngles);
+
+        // Utiliser la rotation en Y de la tourelle pour ajuster les limites de rotation horizontale
+        float adjustedRotationHorizontale = Mathf.Clamp(rotationY, -limiteRotationHorizontale, limiteRotationHorizontale);
+
+        GameObject clone = Instantiate(_projectile, barrel.position, Quaternion.LookRotation(shootingDirection));
+        clone.GetComponent<Rigidbody>().AddForce(shootingDirection * 1500);
         Destroy(clone, 10);
     }
 }
