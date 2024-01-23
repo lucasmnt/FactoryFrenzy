3a02bc597719e0f6f76ebba1bfff796ce2ec83a8
lucas <lucas.menot@viacesi.fr>
2024-01-23 09:47:57 +0100

    bumper trap uploaded

--- Assets/Scripts/World/Traps/Bumper.cs
+++ Assets/Scripts/World/Traps/Bumper.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Bumper : EditorObjects
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] string playerTag;
+    [SerializeField] float baseBounceForce = 5f;
+    [SerializeField] float velocityMultiplier = 0.2f;
+    [SerializeField] int minFramesToApplyForce = 5;
+    [SerializeField] int maxFramesToApplyForce = 30;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.transform.CompareTag(playerTag))
+        {
+            Rigidbody otherRB = collision.rigidbody;
+
+            // Obtenir la direction opposée à la normale de la collision
+            Vector3 forceDirection = -collision.contacts[0].normal;
+            Vector3 exemple = new Vector3(forceDirection.x, forceDirection.y/10, forceDirection.z);
+
+            // Ajuster la magnitude de la force en fonction de la vélocité actuelle du joueur
+            float finalBounceForce = baseBounceForce+(otherRB.velocity.magnitude*velocityMultiplier);
+
+            // Calculer le nombre de frames en fonction de la vélocité relative
+            int framesToApplyForce = Mathf.Clamp((int)(otherRB.velocity.magnitude*0.1f), minFramesToApplyForce, maxFramesToApplyForce);
+
+            // Appliquer la force en direction opposée avec la force spécifiée sur plusieurs frames
+            StartCoroutine(ApplyForceOverFrames(otherRB, exemple*finalBounceForce, framesToApplyForce));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ApplyForceOverFrames(Rigidbody rb, Vector3 force, int frames)
     {
-        
+        for (int i = 0;i<frames;i++)
+        {
+            // Appliquer une fraction de la force à chaque frame
+            rb.AddForce(force/frames, ForceMode.Impulse);
+            yield return null;
+        }
     }
 }
